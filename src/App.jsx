7a3b7070f85d1ee22d46db6197055f5547016dcf
jieunsse <jieunsse@gmail.com>
2024-02-10 18:12:41 +0900

    first commit for boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/normalize.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      {/*<Routes><Route path="/" element={} /></Routes>*/}
+      <div>솔챌을 위한 보일러 플레이트</div>
+    </>
+  );
+}
+
+export default App;
