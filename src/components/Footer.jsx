32f6e3a4162983719061ebe15c5dd5ab8df9a704
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-15 11:26:06 +0900

    footer 디자인 다시 제작

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
-import { FaHome, FaBars } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import { GrPrevious } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { MdOutlineCheckBoxOutlineBlank } from 'react-icons/md';
 
 function Footer() {
   const navigate = useNavigate();
@@ -11,10 +12,13 @@ function Footer() {
   }
 
   return (
-    <footer className="fixed bottom-0 max-w-md w-full border-t flex justify-between items-center p-5 bg-white">
+    <footer
+      className="fixed bottom-0 max-w-md w-full flex justify-between items-center p-5"
+      style={{ backgroundColor: '#E4E4E4' }}
+    >
       <FaBars className="text-gray-600 cursor-pointer" />
       <Link to="/" className="text-gray-600 text-xl">
-        <FaHome className="" />
+        <MdOutlineCheckBoxOutlineBlank />
       </Link>
       <GrPrevious onClick={goBack} className="cursor-pointer" />
     </footer>
