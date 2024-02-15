49d13f33372a1de6bdf2ebf4e307f84e6f662674
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-15 17:07:53 +0900

    max-w를 500px로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="max-w-md h-screen border">
+    <div className="max-w-[500px] h-screen border m-auto">
       <Navbar />
       <Outlet />
       <Footer />
