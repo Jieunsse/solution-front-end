a4f49a9cb36d9b678416b51b2a0d28fa729b0644
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-15 09:45:42 +0900

    Navbar 디자인 및버튼 구현

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+import logo from '../../public/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useState, useEffect, useRef } from 'react';
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const closeMenu = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', closeMenu);
+
+    return () => {
+      document.removeEventListener('mousedown', closeMenu);
+    };
+  }, []);
+
+  const handleShow = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <nav className="flex top-0 items-center border-b h-12 fixed w-full max-w-md bg-white">
+      <FaBars onClick={handleShow} className="w-12 h-5 absolute" />
+      {isOpen && (
+        <div
+          ref={menuRef}
+          className="absolute top-10 left-3 flex flex-col w-32 gap-2"
+        >
+          <Link
+            to="/noticeBoard"
+            className="text-md border-2 pl-2 rounded-md bg-white"
+            onClick={() => setIsOpen(false)}
+          >
+            📋 게시판
+          </Link>
+          <Link
+            to="/write"
+            className="border-2 pl-2 rounded-md bg-white"
+            onClick={() => setIsOpen(false)}
+          >
+            📝 게시판 작성
+          </Link>
+        </div>
+      )}
+      <Link to="/" className="flex justify-center flex-1">
+        <img src={logo} />
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
