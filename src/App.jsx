687c8050972dac077ab5c27a2eacd5d18dd24f19
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-16 19:53:09 +0900

    App.jsx 한테 높이 설정해서 전역적으로 관리

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="max-w-[500px] h-screen border m-auto">
+    <div className="max-w-[500px] h-screen border m-auto"  
+      style={{
+        maxHeight: `calc(100vh - 60px )`,
+        paddingTop: '66px',
+    }}>
       <Navbar />
       <Outlet />
       <Footer />
