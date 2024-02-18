f6b261d23df1e1066b6baf87b1d5cec0551528da
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-18 15:18:19 +0900

    첫 로딩창 레이아웃 생성 및 다자인

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Logo from '../public/logo.svg';
 import './styles/normalize.css';
 import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="max-w-[500px] h-screen border m-auto"  
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+      navigate('/login');
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return isLoading ? (
+    <div className="max-w-[500px] h-screen border m-auto bg-black">
+      <img src={Logo} className="w-[287px] flex m-auto h-screen" />
+    </div>
+  ) : (
+    <div
+      className="max-w-[500px] h-screen border m-auto"
       style={{
         maxHeight: `calc(100vh - 60px )`,
         paddingTop: '86px',
-    }}>
+      }}
+    >
       <Navbar />
       <Outlet />
       <Footer />
