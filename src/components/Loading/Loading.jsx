6a65f62e65219c94ac2d41642bc79128d08d32bd
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-18 15:24:42 +0900

    처음 로딩 화면 구현 및 홈 디자인

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+import Logo from '../../../public/logo.svg';
+
+const Loading = () => {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img src={Logo} />
+      </div>
+    </>
+  );
+};
+
+export default Loading;
